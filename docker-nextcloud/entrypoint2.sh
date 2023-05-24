19ce3e7557ba37706705ac1c329ba8a181d2c605
Evgen Rusakov <revgen@users.noreply.github.com>
2023-05-23 23:19:27 -0400

    Save

--- docker-nextcloud/entrypoint2.sh
+++ docker-nextcloud/entrypoint2.sh
@@ -3,6 +3,7 @@ echo "Execute a custom $0 script"
 if [ -n "${PGID}" ] && [ -n "${PGID}" ]; then
     WWW_USER=www-data
     echo "Update ${WWW_USER} UID=${PUID} and GID=${PGID}"
+    groupmod -g 120 dialout
     groupmod -g ${PGID} "${WWW_USER}"
     usermod -u ${PUID} -g ${PGID} "${WWW_USER}"
     echo "Update ${WWW_USER} UID=${PUID} and GID=${PGID} - Done"
